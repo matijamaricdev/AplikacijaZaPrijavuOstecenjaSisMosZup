b1bc8d20afb0a8f27bba5e28bf5239fbac4b4d2f
matijamaricdev <matijamaricdev@gmail.com>
2021-01-31 01:39:42 +0100

    Added reference and comment

--- Models/Prijavitelj.cs
+++ Models/Prijavitelj.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,8 @@ namespace AplikacijaZaPrijavuOstecenjaSisMosZup.Models
 {
     public class Prijavitelj
     {
+        //Unique Id
+        [Key]
         public int Id { get; set; }
         public string Ime { get; set; }
         public string Prezime { get; set; }
